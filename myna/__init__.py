b0888232e0ebcffdbcd6ccbac50fd9dfb38aabd6
Yuichiro Smith <contact@yu-smith.com>
2021-10-13 23:32:40 +0900

    :bookmark: v0.1.0

---
+++ myna/__init__.py
@@ -0,0 +1,7 @@
+__copyright__ = 'Copyright (C) 2021 Your Name'
+__version__ = '0.1.0'
+__license__ = 'Apache Licence 2.0'
+__author__ = 'Yuichiro Smith'
+__author_email__ = 'contact@yu-smith.com'
+__url__ = 'https://github.com/yu-ichiro/myna-python'
+
